690c36d48dc871a7ae169703f82f6addbead96f1
Omar <omar2535@gmail.com>
2018-11-01 21:20:11 -0700

    added link to for navbar

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
 
+// link to tags prevenst redirecting requests to server
+// so that no reloading is done
 const Navbar = ()=>{
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo">Poke' Times</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </div>
         </nav>
